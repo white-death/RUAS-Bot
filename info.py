211eeaf8c09ffffd067fc8585d78ee02c01c2675
white-death <tester.whitedeath@gmail.com>
2020-12-23 08:09:13 +0530

    Adding info.py for Timetable and Class links

---
+++ info.py
@@ -0,0 +1,79 @@
+#class timing infosss...  (start time, end time, links)
+
+
+#CE = Class Ends (1, 2, etc..)  ==>  CE1, CE2, etc...
+
+CE1 = ("09:55")
+CE2 = ("11:15")
+CE3 = ("12:05")
+CE4 = ("12:55")
+CE5 = ("14:30")
+CE6 = ("15:20")
+CE7 = ("16:10")
+CE8 = ("17:00")
+
+
+#CS = Class Starts (1, 2, etc..)  ==>  CS1, CS2, etc...
+
+CS1 = ("09:10") 
+#actual class at 9:05
+
+monCI = ("https://")
+wedCV = ("https://")
+
+CS2 = ("10:00") 
+#actual class at 9:55
+
+tueCV = ("https://teams.microsoft.com/")
+#wedWA = ("https://")
+
+CS3 = ("11:20") 
+#actual class at 11:15
+
+monCV = ("https://")
+tueCI = ("https://")
+wedDM = ("https://teams.microsoft.com/")
+
+CS4 = ("12:10") 
+#actual class at 12:05
+
+monDM = ("https://")
+
+CS5 = ("13:00") 
+#actual class at 12:55
+
+wedCI = ("https://")
+thuCI = ("https://teams.microsoft.com/")
+
+CS6 = ("14:35") 
+#actual class at 14:30
+
+monCV2 = ("https://")
+tueDM = ("https://")
+wedWA2 = ("https://")
+thuWA = ("https://")
+friWA = ("https://")
+
+CS7 = ("15:25") 
+#actual class at 15:20
+
+tueWA = ("https://")
+
+CS8 = ("16:15") 
+#actual class at 16:10
+
+tueCI2 = ("https://")
+wedCV2 = ("https://")
+#CSxtra1 = ("")
+#CSxtra2 = ("")
+
+
+#subject names...
+
+ci = ("Computational Intelligence")
+cv = ("Computer Vision")
+wa = ("Web Architecture")
+dm = ("Data Mining")
+
+#tt = ("15:18")
+
